79f22c0fff7789870fcfc68d736271a8c4ef5fe1
kousar-21 <kousarahammed5@gmail.com>
2025-10-09 12:00:42 +0600

    adding navbar part

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import navIcon from "../../assets/nab/nav_icon.png"
 
 const Navbar = () => {
+    const links = <>
+        <li><a>About</a></li>
+        <li><a>Services</a></li>
+        <li><a>Projects</a></li>
+        <li><a>Contact</a></li>
+    </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-100 pt-[50px] px-[50px]">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -10,25 +17,24 @@ const Navbar = () => {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 3</a></li>
-                        <li><a>Item 3</a></li>
-                        <li><a>Item 3</a></li>
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-[#000000] text-base">
+                        {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <div>
+                    <h3 className='text-[#000000] text-base'>TechsaByte</h3>
+                </div>
+
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 3</a></li>
-                    <li><a>Item 3</a></li>
-                    <li><a>Item 3</a></li>
+
+                <img className='text-[#000000] size-4 mr-[8.5px]' src={navIcon} alt="TechsaByte-Icon" />
+                <ul className="menu menu-horizontal px-1 text-[#000000] text-base">
+                    {links}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn bg-[#00000010] border-none hover:bg-gray-300">Set Meeting</a>
             </div>
         </div>
     );
